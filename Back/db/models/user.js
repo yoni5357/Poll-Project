3d9c5dcc8bd8392572d9c1df49e8d735c95dc014
Yonatan Gorovetz <goro5357@gmail.com>
2025-10-10 17:57:21 +0300

    added password hashing at model layer with bcrypt

--- Back/db/models/user.js
+++ Back/db/models/user.js
@@ -1,13 +1,29 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email:    { type: DataTypes.STRING(255), allowNull: false, unique: true },
-    username: { type: DataTypes.STRING(100), allowNull: false, unique: true },
-    password: { type: DataTypes.STRING(255), allowNull: false },
-  }, {
-    tableName: 'users',
-    underscored: true,          // matches created_at / updated_at
-    timestamps: true,           // uses created_at / updated_at with underscored
+  const User = sequelize.define(
+    "User",
+    {
+      email: { type: DataTypes.STRING(255), allowNull: false, unique: true },
+      username: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+      password: { type: DataTypes.STRING(255), allowNull: false },
+    },
+    {
+      tableName: "users",
+      underscored: true, // matches created_at / updated_at
+      timestamps: true, // uses created_at / updated_at with underscored
+    }
+  );
+
+  User.addHook("beforeCreate", async (user) => {
+    const bcrypt = require("bcrypt");
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+
+  User.addHook("beforeUpdate", async (user) => {
+    if (user.changed("password")) {
+      const bcrypt = require("bcrypt");
+      user.password = await bcrypt.hash(user.password, 10);
+    }
   });
 
   return User;
