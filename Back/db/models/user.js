2d604af9eab11aa75d6489c47e46dc7fed8eec9f
Yonatan Gorovetz <goro5357@gmail.com>
2025-10-10 17:55:13 +0300

    added user model

---
+++ Back/db/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email:    { type: DataTypes.STRING(255), allowNull: false, unique: true },
+    username: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    password: { type: DataTypes.STRING(255), allowNull: false },
+  }, {
+    tableName: 'users',
+    underscored: true,          // matches created_at / updated_at
+    timestamps: true,           // uses created_at / updated_at with underscored
+  });
+
+  return User;
+};
